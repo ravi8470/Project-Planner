7e34390af05513eb75d4e76367c765ae28961bb3
ravi8470 <rvkmr0851@gmail.com>
2019-05-21 01:23:00 +0530

    added data for projects from reducer's state store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title:'sdf sdfsdf', content:'sdkfjsdfj sdklfjsdj'},
+        {id: '2', title:'sdfds 32324r', content:'sdkfjsdfj sdklfjsdjsdkfjsdfj sdklfjsdj'},
+        {id: '3', title:'32 4522', content:'sdkfjsdfj sdk'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state;
