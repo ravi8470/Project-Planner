5ce6b0833202414b5d42b1997bc506edceb50024
ravi8470 <rvkmr0851@gmail.com>
2019-05-21 03:31:19 +0530

    saving a new project to firestore DB

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,7 +2,20 @@
 export const createProject = (project) => {
     // this is the functionality of thunk, it allows us to return a function instead of an object
     return (dispatch, getState, {getFirebase, getFirestore}) => {
-        //make async call to db
-        dispatch({type: 'CREATE_PROJECT', project: project})
+        //initialise getFirestore and store the result in a fn which gives a reference to fb DB
+        const firestore = getFirestore();
+        // adding doc to firestore. its an async op and returns a promise
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Ravi',
+            authorLastName: 'Kumar',
+            authorId: 1234,
+            createdAt: new Date()
+        }).then(() =>{
+            dispatch({type: 'CREATE_PROJECT', project: project});
+        }).catch(err =>{
+            dispatch({type:'CREATE_PROJECT_ERROR',err});
+        })
+        
     }
 }
