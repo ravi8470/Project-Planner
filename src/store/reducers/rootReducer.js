13ca9c13c42065aec42550b721fdbdbb5642cc3e
ravi8470 <rvkmr0851@gmail.com>
2019-05-21 03:54:22 +0530

    added sync for project data from firestore to Dashboard state variable

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,9 +2,13 @@ import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import {combineReducers} from 'redux'
 
+//this is a pre-made reducer for syncing firestore data with our state in the background
+import {firestoreReducer} from 'redux-firestore'
+
 const rootReducer = combineReducers({
     auth: authReducer,
-    project: projectReducer
+    project: projectReducer,
+    firestore: firestoreReducer
 })
 
 export default rootReducer;
