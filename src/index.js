ea904667990e523a084280523e28ce17e362cbba
ravi8470 <rvkmr0851@gmail.com>
2019-05-21 02:54:33 +0530

    added react-redux-firebase & redux-firestore and used their respective enhancers to use firebase functionality in  action creator

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux';
+
+//import compose to combine several storeEnhancers
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+//to access firestore and firebase apis in action creators
+import {reduxFirestore ,getFirestore} from 'redux-firestore';
+import {reactReduxFirebase ,getFirebase} from 'react-redux-firebase';
+
+//this needs to be passed to the reduxFirestore and reactReduxFirebase store enhancers so that getFirestore and getFirebase know what to connect to when used in action creator.
+import fbConfig from './config/fbConfig';
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,7 @@
 //to handle any kind of actions related to a proj such as adding or deleting a project
 export const createProject = (project) => {
     // this is the functionality of thunk, it allows us to return a function instead of an object
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         //make async call to db
         dispatch({type: 'CREATE_PROJECT', project: project})
     }
