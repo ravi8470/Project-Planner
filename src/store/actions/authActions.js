26427f30a0040dcb3c206841163331ebd6ab2fcd
ravi8470 <rvkmr0851@gmail.com>
2019-05-21 14:18:25 +0530

    implement signup

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -18,3 +18,23 @@ export const signOut = () => {
         firebase.auth().signOut().then(() => { dispatch({type:'SIGNOUT_SUCCESS'})})
     }
 }
+
+export const signUp = (newUser) => {
+    return (dispatch, getState,{getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+        ).then((resp) => {
+            return firestore.collection('users').doc(resp.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            })
+        }).then( () => {
+            //this resolve runs after the set method in the last then has resolved
+            dispatch({type:'SIGNUP_SUCCESS'})
+        }).catch(err => dispatch({type:'SIGNUP_ERROR',err}))
+    }
+}
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -17,10 +17,24 @@ const authReducer = (state = initState, action) => {
                 ...state,
                 authError: null
             }
-            
+
         case 'SIGNOUT_SUCCESS':
             console.log('sign out success');
             return state;
+
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success');
+            return {
+                ...state,
+                authError: null
+            }
+
+        case 'SIGNUP_ERROR':
+            console.log('signup error: ' + action.err);
+            return {
+                ...state,
+                authError: action.err.message
+            }
 
         default: return state;
     }
