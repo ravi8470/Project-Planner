43d0dac7b5d7c72dd3812f37a84ba7ba7311c989
ravi8470 <rvkmr0851@gmail.com>
2019-05-22 00:16:27 +0530

    added the notification for project created by writing an object to a document in the notifications collection. The collection gets created automatically.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+//added to use the admin SDKs to interact with services like auth service and firestore service
+admin.initializeApp(functions.config().firebase)
+
+//trigger here is onRequest so this runs when there is a browser request to its URL
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications').add(notification).then( doc => {
+        console.log('notification added', doc);//this logs to the firebase functions log tab.
+    })
+})
+
+
+//trigger here is onCreate
+exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate(doc => {
+    const newProject = doc.data();
+    const notification = {
+        notif_type: 'Added a new Project',
+        user: `${newProject.authorFirstName} ${newProject.authorLastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    return createNotification(notification);
+})
